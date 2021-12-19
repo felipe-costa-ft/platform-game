8cbf0330fa54c536ae30d5542094e441aa8f220a
felipe-costa-ft <felipecosta@ieee.org>
2021-12-19 16:11:23 -0300

    Initial project structure

---
+++ src/config.js
@@ -0,0 +1,24 @@
+import Phaser from "phaser";
+
+export default {
+  type: Phaser.AUTO,
+  parent: "game",
+  backgroundColor: "#50bbff",
+  scale: {
+    width: 500,
+    height: 320,
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
+  render: {
+    pixelArt: true,
+  },
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 750 },
+      debug: false,
+    },
+  },
+  debugMode: true,
+};
---
+++ src/scenes/Game.js
@@ -0,0 +1,11 @@
+import Phaser from "phaser";
+
+class Game extends Phaser.Scene {
+  preload() {}
+
+  create() {}
+
+  update(time, delta) {}
+}
+
+export default Game;
